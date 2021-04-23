8063c3b99bb08c26731791845ba3c0ed0795c0f5
Nikolai Osipov <Ayakashi2.0@yandex.ru>
2021-04-23 19:39:01 +0300

    start

---
+++ src/load.js
@@ -0,0 +1,13 @@
+function loadScript(src, callback) {
+    const el = document.createElement('script');
+    el.src = src;
+    el.type = 'text/javascript';
+    el.onload = callback;
+
+    document.body.appendChild(el);
+}
+
+
+loadScript('src/alarmclock.js', () => {
+    console.log('Script loaded')
+});
