8c6c9472922b67b59d9f253befc4dc4dee28d067
Nikolai Osipov <Ayakashi2.0@yandex.ru>
2021-05-16 14:31:14 +0300

    load.js

--- src/load.js
+++ src/load.js
@@ -1,13 +1,40 @@
-function loadScript(src, callback) {
-    const el = document.createElement('script');
-    el.src = src;
-    el.type = 'text/javascript';
-    el.onload = callback;
+// function loadScript(src, callback) {
+//     const el = document.createElement('script');
+//     el.src = src;
+//     el.type = 'text/javascript';
+//     el.onload = callback;
 
-    document.body.appendChild(el);
-}
+//     document.body.appendChild(el);
+// }
 
 
-loadScript('src/alarmclock.js', () => {
-    console.log('Script loaded')
-});
+// loadScript('src/alarmclock.js', () => {
+//     console.log('Script loaded')
+// });
+
+
+function loadScript(urls, callback) {
+    if(typeof urls === "function") return urls();
+    if(typeof urls === "string") urls = [urls];
+  
+    const existedScripts = Array.from(document.getElementsByTagName('script'), elem => elem.src);
+    const promises = [];
+  
+    if (!Array.isArray(urls))
+      throw new TypeError();
+    
+    urls.forEach(url => {
+      if (existedScripts.includes(url)) return;
+      
+      const element = document.createElement("script");
+      element.type = "text/javascript";
+      element.src = url;
+      promises.push(new Promise(resolve => {
+        element.onload = resolve;
+      }));
+      
+      document.body.appendChild(element);
+    });
+  
+    Promise.all(promises).then(callback);
+  }
